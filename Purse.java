717215f6b6148ed3193a42f665b7a048d2b6752a
Nadzirah binti Mokhtar <73456476+NadzirahMokhtar@users.noreply.github.com>
2021-04-17 16:15:16 +0800

    Add files via upload

---
+++ Purse.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Purse {
+	
+	String Brand;
+	String Color;
+	int PriceRM;
+	double VolumeLiters;
+	int Discount;
+	int DiscountPrice;
+	
+	Scanner sc = new Scanner(System.in);
+	
+	Purse() {
+		
+		System.out.printf("%-20s:", "Enter the Brand");
+		this.Brand = sc.nextLine();
+		
+		System.out.printf("%-20s:", "Enter the Color");
+		this.Color = sc.nextLine();
+		
+		System.out.printf("%-20s:", "Enter the Price");
+		this.PriceRM = sc.nextInt();
+		
+		System.out.printf("%-20s:", "Enter the Volume");
+		this.VolumeLiters = sc.nextDouble();
+		
+		System.out.printf("%-20s:", "Enter Discount Percent (%)");
+		this.Discount = sc.nextInt();
+		this.DiscountPrice = (int)((this.PriceRM * (100 - Discount) / 100));
+		
+		
+	}
+	
+	Purse (String br, String color, int price, double volume, int discount){
+		this.Brand = br;
+		this.Color = color;
+		this.PriceRM = price;
+		this.VolumeLiters = volume;
+		this.Discount = discount;
+		this.DiscountPrice = (int)((price * (100 - discount) / 100));
+		
+	}
+}
+
